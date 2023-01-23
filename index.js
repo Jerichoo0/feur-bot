96d928da4b87d8f98487c7a877625078f02a4809
Jerichoo0 <jerichocherijo@gmail.com>
2023-01-23 11:05:28 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,130 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on("ready", () =>{
+    console.log("bot opérationnel");
+});
+
+
+
+client.on("messageCreate", message => {             //évènement déclenché lorsqu'un message est créé
+    if(message.author.bot) return;                  //si l'auteur du message est un bot, on ne fait rien
+    console.log(message);                           //si l'auteur n'est pas un bot, on écrit le message dans la console de débug
+
+    
+    function occurences(str){                                           //on créé la fonction occurences qui compte le nombre de fois qu'une suite de caractère "str" apparaît dans le message
+        let curIndex = 0;                                               //on commence à l'index 0 du message
+        for (let i = 0; i !== -1; i++) {
+            if(message.content.indexOf(str, curIndex) === -1){          //la méthode indexOf renvoie -1 si il ne détecte pas la châine str dans le message à partir de curIndex, dans ce cas on renvoie i
+                return i;
+            } else {                                                    //si la chaîne contient str, indexOf ne renvoie pas -1 
+                curIndex = message.content.indexOf(str, curIndex) + 1;  //dans ce cas on met à jour curIndex pour recommencer la boucle à un index supérieur à celui de la chaîne str précédente
+            }
+        }
+    }
+
+    const banWords = occurences('quoiqu') + occurences('Quoiqu') + occurences('QUOIQU') +occurences('acqui') +occurences('Acqui') +occurences('ACQUI') +occurences('Commenta') +occurences('Commenta') +occurences('COMMENTA');           
+    const de_quoi = occurences('de quoi') + occurences('De quoi') + occurences('dequoi') + occurences('Dequoi');    //on additionne les occurences de plusieurs orthographes qu'on enregistre dans une constante
+    const DE_QUOI = occurences('DE QUOI')
+    const qui = occurences('qui') + occurences('Qui')
+    const QUI = occurences('QUI')
+    const quoi = occurences('quoi') + occurences('Quoi')
+    const QUOI = occurences('QUOI')
+    const comment = occurences('comment') + occurences('Comment')
+    const COMMENT = occurences('COMMENT')
+
+    if(banWords === 0){
+        if(de_quoi > 0){                        //si il y a au moins un "de quoi" on répond
+            let reply = 'De feur'                   //le premier morceau contient la majuscule
+            for(let i=1; i< de_quoi; i++){      //si il y a plus d'un "de quoi" on lance la boucle pour compléter le nombre de feur requis
+                reply = reply + ', de feur'
+            }
+            reply = reply + ' !'                //on termine par le point d'interrogation
+            message.reply(reply)
+        } else if(DE_QUOI > 0){
+            let reply = 'DE FEUR'
+            for(let i=1; i< DE_QUOI; i++){
+                reply = reply + ', DE FEUR'
+            }
+            reply = reply + ' !'
+            message.reply(reply)
+        } else if(qui > 0){
+            let reply = 'Kette'
+            for(let i=1; i< qui; i++){
+                reply = reply + ', kette'
+            }
+            reply = reply + ' !'
+            message.reply(reply)
+        } else if(QUI > 0){
+            let reply = 'KETTE'
+            for(let i=1; i< QUI; i++){
+                reply = reply + ', KETTE'
+            }
+            reply = reply + ' !'
+            message.reply(reply)
+        } else if(quoi > 0){
+            let reply = 'Feur'
+            for(let i=1; i< quoi; i++){
+                reply = reply + ', feur'
+            }
+            reply = reply + ' !'
+            message.reply(reply)
+        } else if(QUOI > 0){
+            let reply = 'FEUR'
+            for(let i=1; i< QUOI; i++){
+                reply = reply + ', FEUR'
+            }
+            reply = reply + ' !'
+            message.reply(reply)
+        } else if(comment > 0){
+            let reply = 'Tateur'
+            for(let i=1; i< comment; i++){
+                reply = reply + ', tateur'
+            }
+            reply = reply + ' !'
+            message.reply(reply)
+        } else if(COMMENT > 0){
+            let reply = 'TATEUR'
+            for(let i=1; i< COMMENT; i++){
+                reply = reply + ', TATEUR'
+            }
+            reply = reply + ' !'
+            message.reply(reply)
+        }
+    }
+})
+
+/*
+ else if(X > 0){
+    let reply = 'Str'
+    for(let i=1; i< X; i++){
+        reply = reply + ', str'
+    }
+    reply = reply + ' !'
+    message.reply(reply)
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login("YOUR TOKEN")      //Put your discord bot token between the ""
